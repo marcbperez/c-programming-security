090d24d51f1da75d5b8cb1b132a2bd22a605e595
marcbperez <marcbperez@users.noreply.github.com>
2017-02-27 23:21:43 +0000

    Added second set of examples.

---
+++ src/source/pointer_types.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+  int i;
+
+  char char_array[5] = {'a', 'b', 'c', 'd', 'e'};
+  int int_array[5] = {1, 2, 3, 4, 5};
+
+  char *char_pointer;
+  int *int_pointer;
+
+  char_pointer = char_array;
+  int_pointer = int_array;
+
+  for (i=0; i<5; i++) {
+    printf("[integer pointer] point to %p, which contains the integer %d\n",
+      int_pointer, *int_pointer);
+    int_pointer = int_pointer + 1;
+  }
+
+  for (i=0; i<5; i++) {
+    printf("[char pointer] point to %p, which contains the char '%c'\n",
+      char_pointer, *char_pointer);
+    char_pointer = char_pointer + 1;
+  }
+}
